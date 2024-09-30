99177f9ae4408bec9f9b5d00864bbd939094a7f1
Jon Guymon <jon.guymon@gmail.com>
2024-09-29 18:45:49 -0700

    no more redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,4 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://everything-counts.guymon.family',
 	integrations: [mdx(), sitemap()],
-	redirects: {
-			'/': '/blog/'
-	}
 });
